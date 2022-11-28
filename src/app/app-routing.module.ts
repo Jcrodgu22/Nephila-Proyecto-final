de11e8f57366b026426e489f6b4f9e944272ee9d
Felipe95T <felipe_calderon_trujillo@hotmail.com>
2022-11-27 20:53:12 -0500

    cambio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
 import { LoginComponent } from './routes/login/login.component';
 import { Page404Component } from './routes/page404/page404.component';
+import { ServicesComponent } from './routes/services/services.component';
 import { StoreComponent } from './routes/store/store.component';
 
 const routes: Routes = [
@@ -11,7 +12,7 @@ const routes: Routes = [
   {path: "home", component: HomeComponent},
   {path: "store", component: StoreComponent},
   {path: "login", component: LoginComponent},
-
+  {path: "services", component: ServicesComponent},
   {path:"**", component: Page404Component },
 ];
 
